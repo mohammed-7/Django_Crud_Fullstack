4de4e50fd56694358c1e278ebf55da0a7d63a26a
mohammed <khuzaif254@gmail.com>
2025-02-10 17:06:57 +0530

    first commits

---
+++ app/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('create', views.create, name='create'),
+    path('delete/<int:id>/', views.delete_task, name='delete_task'),
+    path("get",views.get_task,name='get_task'),
+
+]
---
+++ app/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+from .models import Task
+from django.db import connection
+# Create your views here.
+
+def home(request):
+    return render(request, 'Home.html')
+
+def create(request):
+    if request.method == "POST":
+        task_name = request.POST.get("task_name")
+        print(task_name)
+        if task_name:
+            Task.objects.create(task_name=task_name)
+            data = Task.objects.all()
+            return render(request,"home.html",{"data":data})
+    return render(request,"home.html")
+
+
+
+# def delete_task(request,id):
+#     object = Task.objects.get(id=id)
+#     data = Task.objects.all()
+#     object.delete()
+#     return render(request,"delete.html",{"data":data})
+def delete_task(request,id):
+    object = Task.objects.get(id=id)
+    object.delete()
+    if not Task.objects.all():
+        with connection.cursor() as cursor:
+            cursor.execute("ALTER TABLE app_task AUTO_INCREMENT = 1")        
+    data = Task.objects.all()
+    return render(request,"home.html",{"data":data})
+            
+def get_task(request):
+    data = Task.objects.all()
+    return render(request,'get.html',{"data":data})
